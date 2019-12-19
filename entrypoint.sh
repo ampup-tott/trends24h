9d56dcadcf2dd80193b3c13d391fdd283cd72080
Abel Tran <ampup.tott@gmail.com>
2019-12-19 09:56:24 +0700

    update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [ -z "$NODE_ENV" ]; then
     export NODE_ENV=development
 fi
 
-cp -r -L /tmp/package/node_modules /usr/src/app/node_modules
-cd /usr/src/app
+cp -r -L /tmp/package/node_modules /usr/src/trends24h/node_modules
+cd /usr/src/trends24h
 npm start
