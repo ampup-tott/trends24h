a84f4de9ea557af824fd01bf1ef3348e660ed46a
Abel Tran <ampup.tott@gmail.com>
2019-12-19 10:40:19 +0700

    update redis

--- src/lambda/helper/cache.js
+++ src/lambda/helper/cache.js
@@ -5,6 +5,7 @@ import Redis from 'ioredis';
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASS,
   keyPrefix: process.env.UP_STAGE === 'production' ? '' : 'dev-'
 });
 
